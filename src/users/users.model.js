1f4e82d3b09ef5b34ce8a2b6cc769f136338faf2
Ashdixit382 <ashishdixit601@gmail.com>
2026-07-28 13:22:02 +0530

    feat(users): create user model

---
+++ src/users/users.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+},{
+    timestamps: true,
+  });
+
+// A Mongoose model provides an interface to interact with
+// the MongoDB collection using the defined schema.
+// here User is a object which extends model and have function 
+// like User.find(),User.findOne() etc. 
+
+// mongoose.model(modelName, schema)
+// modelName: Logical model name used by Mongoose.
+// schema: Defines the structure and rules for documents.
+// Mongoose automatically maps "User" to the "users"
+// collection (pluralized and lowercased by default).
+
+  // collection is like a table and document is like a row. 
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
